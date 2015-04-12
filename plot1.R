353c5b3be90f8d33046ba23cdb35b96e2bfbdf96
Elias <cursos.elias@gmail.com>
2015-04-12 23:00:14 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 # Plot1.png
-datos = read.csv("household_power_consumption.txt",as.is= TRUE, na.strings = "?", sep = ";", dec = ".",header = TRUE)
-datos2 = subset.data.frame(datos, Date == "1/2/2007" | Date == "2/2/2007",c(1:9))
+datos <- read.csv("household_power_consumption.txt",as.is= TRUE, na.strings = "?", sep = ";", dec = ".",header = TRUE)
+datos2 <- subset.data.frame(datos, Date == "1/2/2007" | Date == "2/2/2007",c(1:9))
 png(file="plot1.png", width = 480, height = 480)
 hist(datos2$Global_active_power, main="Global Active Power", col="red",xlab="Global Active Power (kilowatts)")
 dev.off()
