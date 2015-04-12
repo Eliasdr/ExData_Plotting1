bf840591a16fc515aaaee9eaa157554980ea6aad
Elias <cursos.elias@gmail.com>
2015-04-12 23:05:36 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -2,20 +2,20 @@
 
 library(graphics)
 
-datos = read.csv("household_power_consumption.txt",as.is= TRUE, na.strings = "?", sep = ";", dec = ".",header = TRUE)
-datos2 = subset.data.frame(datos, Date == "1/2/2007" | Date == "2/2/2007",c(1:9))
+datos <- read.csv("household_power_consumption.txt",as.is= TRUE, na.strings = "?", sep = ";", dec = ".",header = TRUE)
+datos2 <- subset.data.frame(datos, Date == "1/2/2007" | Date == "2/2/2007", c(1:9))
 fecha <- as.Date(datos2$Date, "%d/%m/%Y")
 dia_sem <- weekdays(fecha,TRUE)
-longitud = length(datos2$Global_active_power)
+longitud <- length(datos2$Global_active_power)
 
 png(file="plot4.png", width = 480, height = 480)
 
-par(mfcol=c(2,2))
+par(mfcol=c(2, 2))
 
-plot(datos2$Global_active_power,type="l",ylab="Global Active Power kilowatts)",axes=FALSE,frame.plot=TRUE,xlab='')
+plot(datos2$Global_active_power,type = "l", ylab = "Global Active Power kilowatts)", axes=FALSE, frame.plot=TRUE, xlab='')
 axis(2)
-longitud = length(datos2$Global_active_power)
-axis(1,labels=c("Thu","Fri","Sat"), at=c(0,longitud/2,longitud))
+longitud <- length(datos2$Global_active_power)
+axis(1, labels = c("Thu", "Fri", "Sat"), at = c(0, longitud/2, longitud))
 dev.cur()
 
 plot(datos2$Sub_metering_1,col='black',type='l',axes=FALSE,frame.plot=TRUE,ylab='Energy sub metering',xlab='')
